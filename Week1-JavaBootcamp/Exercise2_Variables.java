b89bbb1d2ae899fabf2c4d1624e7284ca50c3864
Burak Aydin <114289861+burak0aydin@users.noreply.github.com>
2022-09-25 00:17:03 +0300

    Add files via upload

---
+++ Week1-JavaBootcamp/Exercise2_Variables.java
@@ -0,0 +1,15 @@
+public class Exercise2_Variables {
+
+    public static void main(String[] args) {
+        int ogrenciSayisi = 10;
+
+        String  mesaj = "Öğrenci sayısı : ";
+
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("Öğrenci sayısı : " + ogrenciSayisi);
+
+    }
+}
+
+
