0c7367df42855e989f7ed1ede7eea2cfd9011823
Burak Aydin <114289861+burak0aydin@users.noreply.github.com>
2022-09-25 01:43:41 +0300

    Create Exercise8_Arrays.java

---
+++ Week1-JavaBootcamp/Exercise8_Arrays.java
@@ -0,0 +1,20 @@
+public class Exercise8_Arrays {
+    public static void main(String[] args) {
+        
+        String[] ogrenciler = new String[3];
+        ogrenciler[0] = "Engin";
+        ogrenciler[1] = "Ömer";
+        ogrenciler[2] = "Osman";
+        
+        for(int i = 0; i < ogrenciler.length; i++){
+            System.out.println(ogrenciler[i]);
+        }
+        
+        System.out.println("*************************");
+        
+        for(String ogrenci : ogrenciler){
+            System.out.println(ogrenci);
+        }
+    }
+    
+}
