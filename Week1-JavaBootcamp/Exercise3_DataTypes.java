509680cc557cdb2dabc65eed9af118f231d35415
Burak Aydin <114289861+burak0aydin@users.noreply.github.com>
2022-09-25 01:01:03 +0300

    Create Exercise3_DataTypes.java

---
+++ Week1-JavaBootcamp/Exercise3_DataTypes.java
@@ -0,0 +1,13 @@
+public class Exercise3_DataTypes {
+    public static void main(String[] args) {
+        
+        int ogrenciSayisi = 10;
+        
+        String  mesaj = "Öğrenci sayısı : ";
+        
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("Öğrenci sayısı : " + ogrenciSayisi);
+        
+    }
+}
