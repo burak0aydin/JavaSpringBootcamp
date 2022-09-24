55842772d4790c53ad87c60957e5025c79fdba38
Burak Aydin <114289861+burak0aydin@users.noreply.github.com>
2022-09-25 01:24:53 +0300

    Create Exercise3_DataTypes.java

---
+++ Week1-JavaBootcamp/Exercise3_DataTypes.java
@@ -0,0 +1,12 @@
+public class Exercise3_DataTypes {
+    public static void main(String[] args) {
+        double sayi = 18.2;
+        sayi = -129;
+        
+        char karakter = 'A';
+        
+        boolean dogruMu = false;
+        
+        
+    }
+}
