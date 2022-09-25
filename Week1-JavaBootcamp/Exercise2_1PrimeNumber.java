9b324b95b715bf9499f650d15373bf9cf8669198
Burak Aydin <114289861+burak0aydin@users.noreply.github.com>
2022-09-25 12:18:22 +0300

    Create Exercise2_1PrimeNumber.java

---
+++ Week1-JavaBootcamp/Exercise2_1PrimeNumber.java
@@ -0,0 +1,52 @@
+public class Exercise2_1PrimeNumber {
+
+	public static void main(String[] args) {
+
+		int j = 2;
+		int number = 5;
+		boolean isPrime = true;
+		if (number <= 1) 
+		{
+			isPrime = false;
+			System.out.println("not prime");
+		}
+		while (j <= number / 2) 
+		{
+			if (number % j == 0) 
+			{
+				isPrime = false;
+				break;
+			}
+			j++;
+		}
+
+		System.out.println("Is the number prime ? - " + isPrime);
+		
+    /*
+		 	int number=2;
+	        int remainder = number % 2;
+	        
+	        boolean isPrime= true;
+	        if(number ==1)
+	            System.out.println("Sayı asal değildir..");
+	        
+	        if(number <1)
+	        {
+	            System.out.println("Geçersiz Sayı!");
+	            return;
+	        }
+	        
+	        for (int i = 2; i < number ; i++)
+	        {
+	            if(number % i == 0)
+	                isPrime = false;
+	        }
+	        if(isPrime)
+	            System.out.println("Sayı asaldır.");
+	        else{
+	            System.out.println("Sayı asal değildir.");
+	        }
+		 */
+	}
+
+}
