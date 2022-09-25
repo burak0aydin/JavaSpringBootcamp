a0d414d7ae60bb93ebe861c6cb0bb1c24f73aa66
Burak Aydin <114289861+burak0aydin@users.noreply.github.com>
2022-09-25 12:48:19 +0300

    Create Exercise2_4FriendshipOfNumbers.java

---
+++ Week1-JavaBootcamp/Exercise2_4FriendshipOfNumbers.java
@@ -0,0 +1,27 @@
+public class ExampleProject4 {
+
+	public static void main(String[] args) {
+		int number1 = 220;
+		int number2 = 284;
+		int sum1 = 0;
+		int sum2 = 0;
+
+		for (int i = 1 ; i < number1 ; i++) 
+		{
+			if (number1 % i == 0)
+				sum1 += i;
+		}
+		for (int i = 1 ; i < number2 ; i++) 
+		{
+			if (number2 % i == 0)
+				sum2 += i;
+		}
+
+		if (number1 == sum2 && number2 == sum1)
+			System.out.println("These two numbers are friends.");
+		else
+			System.out.println("These two numbers are not friends.");
+
+	}
+
+}
